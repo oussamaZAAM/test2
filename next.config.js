8ef55e34f8584d6e0b15521ff2feeee80570f5f7
oussamaZAAM <zaam.oussama@gmail.com>
2023-07-11 15:45:40 +0100

    Deploy with NestJS

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    /* config options here */
+    output: 'export',
+}
 
 module.exports = nextConfig
