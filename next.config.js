e2c7e7e8f92dd4154788901e897e77375369957c
oussamaZAAM <zaam.oussama@gmail.com>
2023-07-11 15:57:30 +0100

    Remove image optimization

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@
 const nextConfig = {
     /* config options here */
     output: 'export',
+    images: { unoptimized: true } 
 }
 
 module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
       },
       boxShadow: {
         'formation-unhover': '0 0 10px 1px rgba(0, 0, 0, 0.1)',
-        'formation-hover': '4px 4px 10px 1px rgba(0, 0, 0, 0.1)',
+        'formation-hover': '0 0 15px 5px rgba(0, 0, 0, 0.1)',
         'formation-card': '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
       }
     },
