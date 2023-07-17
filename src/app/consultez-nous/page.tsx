b2046e03ff72cd8fc1f4876b67fba90d4abd571b
oussamaZAAM <zaam.oussama@gmail.com>
2023-07-17 16:32:28 +0100

    Apply changes to Consultez-nous Page

--- src/app/consultez-nous/page.tsx
+++ src/app/consultez-nous/page.tsx
@@ -7,13 +7,17 @@ import Navbar from '@/components/Navbar';
 type Props = {}
 
 export default function page({ }: Props) {
+
+    const handleSendContact = () => {
+        console.log('consultez-nous page')
+    }
     return (
         <div className="bg-ac-gray flex flex-col justify-start items-center w-full">
             <Navbar />
 
             {/* Contact Us */}
             <div className="flex justify-center items-center w-full bg-ac-gray -translate-y-6 rounded-t-3xl ">
-                <ContactUsForm type='contact' />
+                <ContactUsForm type="contact" handleSend={handleSendContact} />
             </div>
 
             <Footer />
