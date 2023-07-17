b4792e384bacf95dc33d84f1ababe17d5409864a
oussamaZAAM <zaam.oussama@gmail.com>
2023-07-17 15:58:51 +0100

    Fix problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,47 @@
 "use client";
 
-import ContactUsForm from '@/components/ContactUsForm';
+import ContactUsForm from '@/components/ContactUsForm'
 import Footer from '@/components/Footer';
-import Hero from '@/components/Hero';
-import { IBM_Plex_Sans, Lato, Montserrat } from 'next/font/google';
-import Image from 'next/image';
-import Link from 'next/link';
-import Navbar from '../components/Navbar';
+import Navbar from '@/components/Navbar';
+import { namifySlug } from '@/utils/functions';
+import { DevisInputs } from '@/utils/interfaces';
+import { useSearchParams } from 'next/navigation';
+import React from 'react'
 
-const montserratFont = Montserrat({ weight: "400", subsets: ["latin"] });
-const latoFont = Lato({ weight: "400", subsets: ["latin"] });
-const ibmFont = IBM_Plex_Sans({ weight: "700", subsets: ["latin"] });
+type Props = {}
 
-export default function Home() {
-  return (
-    <div className="flex flex-col justify-center items-center bg-ac-gray w-full">
-      {/* Navbar  */}
-      <Navbar />
+export default function Devis({ }: Props) {
+    // Prendre le nom de la formation d'aupres l'url (params)
+    const searchParams = useSearchParams();
 
-      {/* Hero  */}
-      <Hero />
+    const formation = searchParams.get("formation") !== null ? searchParams.get("formation")! : "";
 
-      {/* Nos Services */}
-      <div id='services' className="flex justify-center items-center bg-ac-bleu rounded-t-3xl w-full pb-28 px-4 pt-20">
-        <div className="flex flex-col justify-start items-center gap-8 xm:gap-16">
-          <h5 className={montserratFont.className + " font-medium text-4xl xm:text-5xl text-center text-white"}>Nos Services</h5>
-          <div className="flex flex-col md:flex-row justify-between items-center md:items-start gap-20 md:gap-56">
+    const handleSendDevis = async (args: DevisInputs) => {
+        await fetch("/api", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                formation: args.formation,
+                entreprise: args.entreprise,
+                fullname: args.fullname,
+                telephone: args.telephone,
+                email: args.email,
+                message: args.message,
+            })
+        });
+    }
+    return (
+        <div className="flex flex-col justify-start items-center w-full bg-ac-gray">
+            <Navbar />
+            <div className="flex w-full h-44 bg-ac-bleu rounded-b-3xl -mb-28"></div>
 
-            {/* Formations Service  */}
-            <div className="flex flex-col justify-start items-center gap-4 max-w-[236px]">
-              <img loading='lazy' decoding='async' className='w-24 h-24' width={96} height={96} src="https://i.ibb.co/DgFnzJ4/formations.png" alt='service-formation' />
-              <h6 className={latoFont.className + " font-semibold text-2xl xm:text-3xl leading-7 text-center text-white"}>Formations</h6>
-              <p className={latoFont.className + " font-medium text-sm xm:text-base leading-5 text-center text-white"}>
-                Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-                consectetur, adipisci velit...
-              </p>
-              <Link href={"/formations"}>
-                <div className="flex justify-center items-center rounded-sm bg-white px-5 py-2 cursor-pointer">
-                  <h6 className={ibmFont.className + " text-ac-bleu uppercase text-xl font-bold"}>Accéder</h6>
-                </div>
-              </Link>
+            <div className="flex justify-center items-center w-full bg-transparent -translate-y-6 rounded-t-3xl">
+                <ContactUsForm type="devis" formation={namifySlug(formation)} handleSendDevis={handleSendDevis} />
             </div>
-            {/* Consulting Service  */}
-            <div className="flex flex-col justify-start items-center gap-4 max-w-[236px]">
-              <Image className='w-24 h-24' width={96} height={96} src="https://i.ibb.co/gy3RfPC/consulting.png" alt='service-formation' />
-              <h6 className={latoFont.className + " font-semibold text-2xl xm:text-3xl leading-7 text-center text-white"}>Consulting</h6>
-              <p className={latoFont.className + " font-medium text-sm xm:text-base leading-5 text-center text-white"}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Sed pulvinar turpis quis sem sodales
-              </p>
-              <Link href={"/consulting"}>
-                <div className="flex justify-center items-center rounded-sm bg-white px-5 py-2 cursor-pointer">
-                  <h6 className={ibmFont.className + " text-ac-bleu uppercase text-xl font-bold"}>Accéder</h6>
-                </div>
-              </Link>
-            </div>
-          </div>
+
+            <Footer />
         </div>
-      </div>
-
-      {/* Contact Us */}
-      <div className="flex justify-center items-center w-full bg-ac-gray -translate-y-6 rounded-t-3xl ">
-        <ContactUsForm type='contact' />
-      </div>
-
-      {/* Footer  */}
-      <Footer />
-    </div>
-  )
+    )
 }
