8f566592bbc9a53ee7749c54f89a4b15e4c17879
oussamaZAAM <zaam.oussama@gmail.com>
2023-07-19 14:50:24 +0100

    Works fine in dev, but needs little tweaks in prod

--- src/app/api/mailing/route.ts
+++ src/app/api/mailing/route.ts
@@ -2,10 +2,10 @@ import { DevisEmail } from "@/emails/devis";
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: NextRequest) {
-
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    
     try {
         const data = await request.json();
         if (process.env.EMAIL_SENDER && process.env.EMAIL_RECEIVER) {
