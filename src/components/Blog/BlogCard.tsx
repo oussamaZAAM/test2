038e6613d76a502d76463290fdcfcfc22fb19793
oussamaZAAM <zaam.oussama@gmail.com>
2023-08-07 14:49:38 +0100

    blog card hover:styling

--- src/components/Blog/BlogCard.tsx
+++ src/components/Blog/BlogCard.tsx
@@ -14,19 +14,20 @@ type Props = {
 export default function BlogCard({ id, title, body }: Props) {
     const firstParagraph = body.find((section) => section.type === "paragraph" || section.type === "header1" || section.type === "header2");
     return (
-        <div className="w-full flex flex-col justify-between items-start py-5 px-6 border-2 gap-4 border-ac-bleu">
+        <Link href={"/blogs/" + id} className="w-full flex flex-col justify-between items-start py-5 px-6 border-2 gap-4 cursor-pointer transition duration-300 border-ac-bleu group text-black hover:text-white hover:bg-ac-bleu">
             <div className="w-full flex flex-col justify-start items-start gap-4">
-                <h3 className={jostFont.className + " font-bold text-2xl text-black text-left line-clamp-2 md:h-16"}>
+                <h3 className={jostFont.className + " font-bold text-2xl text-left line-clamp-2 md:h-16"}>
                     {title}
                 </h3>
-                <h5 className={montserratFont.className + " font-normal text-sm text-black text-left line-clamp-6"}>
+                <h5 className={montserratFont.className + " font-normal text-sm text-left line-clamp-6"}>
                     {firstParagraph ? firstParagraph.text : ""}
                 </h5>
             </div>
-            <Link href={"/blogs/" + id}><p className={montserratFont.className + " w-full text-[#5C8BFC] underline text-left font-semibold text-base"}>
+            <Link href={"/blogs/" + id}>
+                <p className={montserratFont.className + " w-full text-[#5C8BFC] group-hover:text-black underline text-left font-semibold text-base"}>
                 Lire plus
-            </p>
+                </p>
             </Link>
-        </div>
+        </Link>
     )
 }
