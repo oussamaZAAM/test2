f6e07d232f4b65b34e50a7a7425790c1e9aea8c6
oussamaZAAM <zaam.oussama@gmail.com>
2023-08-07 16:09:40 +0100

    More changes to boost SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: pageMetadata.title,
   description: pageMetadata.description,
+  metadataBase: new URL('https://www.aleeconseil.com'),
+  openGraph: {
+    images: '/og-image.png',
+  },
 }
 
 export default function RootLayout({
